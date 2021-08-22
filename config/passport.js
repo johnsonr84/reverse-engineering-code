d8fcd7c92e5ae9f293f0615fff0eb4ebb8bf13a9
Your Name <robertcjohnson1984@gmail.com>
2021-08-22 17:08:34 -0600

    add comment

--- config/passport.js
+++ config/passport.js
@@ -1,49 +1,51 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+// Import packages and modules 
 
-var db = require("../models");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const db = require("../models");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+//Set up passport Local Stragey (use pasport LocalStrategy) and login with user's email and password 
+
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
-  {
-    usernameField: "email"
-  },
-  function(email, password, done) {
-    // When a user tries to sign in this code runs
-    db.User.findOne({
-      where: {
-        email: email
-      }
-    }).then(function(dbUser) {
-      // If there's no user with the given email
-      if (!dbUser) {
-        return done(null, false, {
-          message: "Incorrect email."
+    //user sign in with an email rather than with a username
+    {
+        usernameField: "email"
+    },
+    ((email, password, done) => {
+        //when a user click LOGIN button, this code runs
+        db.User.findOne({
+            where: {
+                email
+            }
+        }).then((dbUser) => {
+            //This cb function will find the correct user from the database and pass it as a closure variable into the callback done(err,user);
+            //So the code in the passport.session() can replace the 'user' value in the req object and pass on to the next middleware in the pile
+            
+            //If there is no user in the database with given email
+            if (!dbUser) {
+                return done(null, false, {message: "Incorrect email."});
+            }
+            //if there is matching email but no matching password in the database
+            if (!dbUser.validPassword(password)) {
+                return done(null, false, {message: "Incorrect password."});
+            } 
+            //If find matching email and password, return the user to the route handler
+            return done(null, dbUser);
         });
-      }
-      // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
-        return done(null, false, {
-          message: "Incorrect password."
-        });
-      }
-      // If none of the above, return the user
-      return done(null, dbUser);
-    });
-  }
+
+    })
 ));
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+//Sequelize serialize and deserialize user in order to help keep authentication state across HTTP requests 
+//Boiler plate needed to make it all work 
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
-// Exporting our configured passport
-module.exports = passport;
+//Export the configured passport 
+module.exports = passport; 
